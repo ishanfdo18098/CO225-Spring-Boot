20de7d23d17cd1f1d3b5021b8ce108d29703a341
Janith Widarshana <e06widu@gmail.com>
2022-02-11 11:49:50 +0530

    Initial Commit

---
+++ src/main/java/com/example/CO225SpringBootApp/Co225SpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.CO225SpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co225SpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Co225SpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CO225SpringBootApp/Co225SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CO225SpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Co225SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
